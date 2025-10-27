d372074ce49615703715d9ffd033922acd9273d9
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2025-10-27 15:12:28 -0300

    refactor to avoid default values

--- mobile/redux/features/linkingSlice.ts
+++ mobile/redux/features/linkingSlice.ts
@@ -1,39 +1,37 @@
 import { Post } from "@gno/types";
 import { GnoNativeApi } from "@gnolang/gnonative";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import * as Linking from "expo-linking";
+import * as Linking from 'expo-linking';
 import { RootState, ThunkExtra } from "redux/redux-provider";
 
 interface State {
-  chainId: string | undefined;
-  remoteURL: string | undefined;
-  txJsonSigned: string | undefined;
-  bech32AddressSelected: string | undefined;
+    txJsonSigned: string | undefined;
+    bech32AddressSelected: string | undefined;
+    chainId: string | undefined;
+    remoteURL: string | undefined;
 }
 
 const initialState: State = {
-  chainId: undefined,
-  remoteURL: undefined,
-  txJsonSigned: undefined,
-  bech32AddressSelected: undefined,
+    txJsonSigned: undefined,
+    bech32AddressSelected: undefined,
+    chainId: undefined,
+    remoteURL: undefined,
 };
 
 export const requestLoginForGnokeyMobile = createAsyncThunk<boolean>("tx/requestLoginForGnokeyMobile", async () => {
-  const url = new URL("land.gno.gnokey://tosignin");
-  url.searchParams.append("callback", "tech.berty.dsocial://signin-callback");
-  url.searchParams.append("client_name", "dSocial");
-  console.log("redirecting to: ", url);
-  return await Linking.openURL(url.toString());
-});
+    const url = new URL('land.gno.gnokey://tosignin');
+    url.searchParams.append('callback', 'tech.berty.dsocial://signin-callback');
+    url.searchParams.append('client_name', 'dSocial');
+    console.log("redirecting to: ", url);
+    return await Linking.openURL(url.toString());
+})
 
 type MakeTxAndRedirectParams = {
-  postContent: string;
-  callerAddressBech32: string;
+    postContent: string,
+    callerAddressBech32: string,
 };
 
-export const postTxAndRedirectToSign = createAsyncThunk<void, MakeTxAndRedirectParams, ThunkExtra>(
-  "tx/makeCallTxAndRedirectToSign",
-  async (props, thunkAPI) => {
+export const postTxAndRedirectToSign = createAsyncThunk<void, MakeTxAndRedirectParams, ThunkExtra>("tx/makeCallTxAndRedirectToSign", async (props, thunkAPI) => {
     const { callerAddressBech32, postContent } = props;
 
     const fnc = "PostMessage";
@@ -44,73 +42,58 @@ export const postTxAndRedirectToSign = createAsyncThunk<void, MakeTxAndRedirectP
     const callbackPath = "/post";
 
     await makeCallTx({ fnc, args, gasFee, gasWanted, callerAddressBech32, reason, callbackPath }, thunkAPI.extra.gnonative);
-  }
-);
+})
 
 type MakeCallTxParams = {
-  packagePath?: string;
-  fnc: string;
-  args: string[];
-  gasFee: string;
-  gasWanted: bigint;
-  send?: string;
-  memo?: string;
-  callerAddressBech32: string;
-  reason: string;
-  callbackPath: string;
+    packagePath?: string,
+    fnc: string,
+    args: string[],
+    gasFee: string,
+    gasWanted: bigint,
+    send?: string,
+    memo?: string,
+    callerAddressBech32: string,
+    reason: string,
+    callbackPath: string,
 };
 
 export const makeCallTx = async (props: MakeCallTxParams, gnonative: GnoNativeApi): Promise<void> => {
-  const {
-    fnc,
-    callerAddressBech32,
-    gasFee,
-    gasWanted,
-    args,
-    packagePath = "gno.land/r/berty/social",
-    reason,
-    callbackPath,
-  } = props;
+    const { fnc, callerAddressBech32, gasFee, gasWanted, args, packagePath = "gno.land/r/berty/social", reason, callbackPath } = props;
 
-  console.log("making a tx for: ", callerAddressBech32);
-  const address = await gnonative.addressFromBech32(callerAddressBech32);
+    console.log("making a tx for: ", callerAddressBech32);
+    const address = await gnonative.addressFromBech32(callerAddressBech32);
 
-  const res = await gnonative.makeCallTx(packagePath, fnc, args, gasFee, gasWanted, address);
+    const res =  await gnonative.makeCallTx(packagePath, fnc, args, gasFee, gasWanted, address)
 
-  const url = new URL("land.gno.gnokey://tosign");
-  url.searchParams.append("chain_id", await gnonative.getChainID());
-  url.searchParams.append("remote", await gnonative.getRemote());
-  url.searchParams.append("tx", res.txJson);
-  url.searchParams.append("update_tx", "true");
-  url.searchParams.append("address", callerAddressBech32);
-  url.searchParams.append("client_name", "dSocial");
-  url.searchParams.append("reason", reason);
-  url.searchParams.append("callback", "tech.berty.dsocial:/" + callbackPath);
+    const url = new URL('land.gno.gnokey://tosign');
+    url.searchParams.append('chain_id', await gnonative.getChainID());
+    url.searchParams.append('remote', await gnonative.getRemote());
+    url.searchParams.append('tx', res.txJson);
+    url.searchParams.append("update_tx", "true");
+    url.searchParams.append('address', callerAddressBech32);
+    url.searchParams.append('client_name', 'dSocial');
+    url.searchParams.append('reason', reason);
+    url.searchParams.append('callback', 'tech.berty.dsocial:/' + callbackPath);
 
-  console.log("redirecting to: ", url);
-  Linking.openURL(url.toString());
-};
+    console.log("redirecting to: ", url);
+    Linking.openURL(url.toString());
+}
 
-export const broadcastTxCommit = createAsyncThunk<void, string, ThunkExtra>(
-  "tx/broadcastTxCommit",
-  async (signedTx, thunkAPI) => {
+export const broadcastTxCommit = createAsyncThunk<void, string, ThunkExtra>("tx/broadcastTxCommit", async (signedTx, thunkAPI) => {
     console.log("broadcasting tx: ", signedTx);
 
     const gnonative = thunkAPI.extra.gnonative;
     const res = await gnonative.broadcastTxCommit(signedTx);
     console.log("broadcasted tx: ", res);
-  }
-);
+});
 
 interface GnodCallTxParams {
-  post: Post;
-  callerAddressBech32: string;
-  callbackPath: string;
+    post: Post,
+    callerAddressBech32: string,
+    callbackPath: string,
 }
 
-export const gnodTxAndRedirectToSign = createAsyncThunk<void, GnodCallTxParams, ThunkExtra>(
-  "tx/gnodTxAndRedirectToSign",
-  async (props, thunkAPI) => {
+export const gnodTxAndRedirectToSign = createAsyncThunk<void, GnodCallTxParams, ThunkExtra>("tx/gnodTxAndRedirectToSign", async (props, thunkAPI) => {
     console.log("gnodding post: ", props.post);
     const { post, callerAddressBech32, callbackPath } = props;
 
@@ -124,35 +107,37 @@ export const gnodTxAndRedirectToSign = createAsyncThunk<void, GnodCallTxParams, 
 
     // await makeCallTx({ fnc, args, gasFee, gasWanted, callerAddressBech32, reason, callbackPath, session }, thunkAPI.extra.gnonative);
     await makeCallTx({ fnc, args, gasFee, gasWanted, callerAddressBech32, reason, callbackPath }, thunkAPI.extra.gnonative);
-  }
-);
+});
 
 /**
  * Slice to handle linking between the app and the GnokeyMobile app
  */
 export const linkingSlice = createSlice({
-  name: "linking",
-  initialState,
-  reducers: {
-    setLinkingData: (state, action) => {
-      const queryParams = action.payload.queryParams;
-      state.bech32AddressSelected = queryParams?.address ? (queryParams.address as string) : undefined;
-      state.txJsonSigned = queryParams?.tx ? (queryParams.tx as string) : undefined;
-      state.chainId = queryParams?.chain_id ? (queryParams.chain_id as string) : undefined;
-      state.remoteURL = queryParams?.remote ? (queryParams.remote as string) : undefined;
+    name: "linking",
+    initialState,
+    reducers: {
+        setLinkingData: (state, action) => {
+            const queryParams = action.payload.queryParams
+
+            state.bech32AddressSelected = queryParams?.address ? queryParams.address as string : undefined
+            state.txJsonSigned = queryParams?.tx ? queryParams.tx as string : undefined
+            state.chainId = queryParams?.chain_id ? queryParams.chain_id as string : undefined
+            state.remoteURL = queryParams?.remote ? queryParams.remote as string : undefined
+        },
+        clearLinking: (state) => {
+            console.log("clearing linking data");
+            state = { ...initialState };
+        }
     },
-    clearLinking: (state) => {
-      console.log("clearing linking data");
-      state = { ...initialState };
-    },
-  },
-  selectors: {
-    selectQueryParamsTxJsonSigned: (state: State) => state.txJsonSigned as string | undefined,
-    selectBech32AddressSelected: (state: State) => state.bech32AddressSelected as string | undefined,
-    selectChainId: (state: State) => state.chainId as string | undefined,
+    selectors: {
+        selectQueryParamsTxJsonSigned: (state: State) => state.txJsonSigned as string | undefined,
+        selectBech32AddressSelected: (state: State) => state.bech32AddressSelected as string | undefined,
+        selectChainId: (state: State) => state.chainId as string | undefined,
+        selectRemoteURL: (state: State) => state.remoteURL as string | undefined,
   },
 });
 
 export const { clearLinking, setLinkingData } = linkingSlice.actions;
 
-export const { selectQueryParamsTxJsonSigned, selectBech32AddressSelected, selectChainId } = linkingSlice.selectors;
+export const { selectQueryParamsTxJsonSigned, selectBech32AddressSelected, selectChainId, selectRemoteURL } =
+  linkingSlice.selectors;
